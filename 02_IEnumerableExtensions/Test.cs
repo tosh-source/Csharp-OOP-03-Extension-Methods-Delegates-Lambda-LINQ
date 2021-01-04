a13233a7451fcdd5eeee9cdfafa07c2fc509f8f9
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-01-04 02:14:53 +0200

    add more tests

--- 02_IEnumerableExtensions/Test.cs
+++ 02_IEnumerableExtensions/Test.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +10,11 @@ namespace Extensions
     {
         static void Main(string[] args)
         {
+            IEnumerable<int> firstType = new [] { 2, 1, 2 };
+            Console.WriteLine(firstType.SumOfCollection());
+
+            IEnumerable<string> secondType = new[] { "2", "1", "2" }; 
+            //Console.WriteLine(secondType.SumOfCollection());  //should throw exception! Only ValueTypes can be calculated!
         }
     }
 }
