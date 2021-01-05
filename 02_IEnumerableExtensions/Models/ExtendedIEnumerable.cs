fec904bd4acd8683db61b2837efbcc880d082748
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-01-05 03:27:00 +0200

    complete extension method CollectionProduct

--- 02_IEnumerableExtensions/Models/ExtendedIEnumerable.cs
+++ 02_IEnumerableExtensions/Models/ExtendedIEnumerable.cs
@@ -11,7 +11,7 @@ namespace Extensions
     {
         public static decimal CollectionSum<T>(this IEnumerable<T> type)
         {
-            decimal result = 0.0m;
+            decimal result = 0;
 
             foreach (var item in type)
             {
@@ -30,7 +30,26 @@ namespace Extensions
 
         public static decimal CollectionProduct<T>(this IEnumerable<T> type)
         {
-            decimal result = 0.0m;
+            decimal result = 0;
+            bool defaultValueIsOne = false;
+
+            foreach (var item in type)
+            {
+                if (result == 0.0m && defaultValueIsOne == false && item is ValueType)
+                {
+                    result = 1;
+                    defaultValueIsOne = true;
+                }
+
+                if (item is ValueType)
+                {
+                    result *= decimal.Parse(item.ToString());
+                }
+                else
+                {
+                    throw new ArgumentException("Only ValueTypes can be calculated!");
+                }
+            }
 
             return result;
         }
