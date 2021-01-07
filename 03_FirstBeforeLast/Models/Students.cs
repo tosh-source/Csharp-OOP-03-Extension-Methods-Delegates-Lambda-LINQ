46a1fae721635e9bc4039749c2efb4d2e446e833
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-01-07 23:05:54 +0200

    complete Students class

--- 03_FirstBeforeLast/Models/Students.cs
+++ 03_FirstBeforeLast/Models/Students.cs
@@ -8,5 +8,20 @@ namespace _03_FirstBeforeLast.Models
 {
     class Students
     {
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
     }
 }
