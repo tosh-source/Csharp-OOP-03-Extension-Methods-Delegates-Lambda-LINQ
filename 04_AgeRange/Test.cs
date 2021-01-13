329a4e84429e8df65b0bee9455ffeb0e27f406d2
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-01-13 02:51:16 +0200

    Problem 4 Age Range.

---
+++ 04_AgeRange/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_AgeRange
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
