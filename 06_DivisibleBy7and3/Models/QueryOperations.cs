4e0859066edad54f936566fa10a27f4e144e6424
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-01-18 22:30:53 +0200

    complete first part of problem 6 - using lambda expressions

--- 06_DivisibleBy7and3/Models/QueryOperations.cs
+++ 06_DivisibleBy7and3/Models/QueryOperations.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace _06_DivisibleBy7and3.Models
 {
-    class QueryOperations
+    public static class QueryOperations
     {
+        public static IEnumerable AllDivisibleNumbsByLambda(this int[] integers)
+        {
+            var result = integers.ToList().FindAll(ints => (ints % 7 == 0 && ints % 3 == 0));
+
+            if (result.Count == 0)
+            {
+                return "No divisible numbers to 7 and 3 in array!";
+            }
+
+            return string.Join(", ", result);
+        }
     }
 }
--- 06_DivisibleBy7and3/Test.cs
+++ 06_DivisibleBy7and3/Test.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _06_DivisibleBy7and3
+namespace _06_DivisibleBy7and3.Models
 {
     class Test
     {
         static void Main(string[] args)
         {
+            int[] integers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(integers.AllDivisibleNumbsByLambda());
+
+            integers = new[] { -21, 7, 3, 17, 30, 35, 49, 42 };
+            Console.WriteLine(integers.AllDivisibleNumbsByLambda());
         }
     }
 }
