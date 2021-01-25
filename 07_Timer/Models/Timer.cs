8d01422039a898805a3f0b811f8d70e67cb7f641
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-01-26 01:18:12 +0200

    first solution variant

---
+++ 07_Timer/Models/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Timer.Models
+{
+    public delegate void TimeTicker();
+
+    public static class Timer
+    {
+        private static int seconds = 0;
+
+        public static int Seconds
+        {
+            get
+            {
+                return seconds;
+            }
+
+            private set
+            {
+                seconds = value;
+            }
+        }
+
+        public static Action startAction = new Action(() => Console.WriteLine(++Seconds));
+    }
+}
---
+++ 07_Timer/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07_Timer.Models
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            TimeTicker ticker = new TimeTicker(Timer.startAction);
+
+            for (int cycles = 1; cycles <= 10; cycles++)
+            {
+                Thread.Sleep(1000);
+                ticker();
+            }
+        }
+    }
+}
