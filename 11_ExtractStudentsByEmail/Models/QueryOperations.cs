3a8f2cfcf38812946d500dd20f6e1bd0e29c06a7
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-02-07 02:08:48 +0200

    complete problem 11

---
+++ 11_ExtractStudentsByEmail/Models/QueryOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SharedClassStudents.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11_ExtractStudentsByEmail.Models
+{
+    class QueryOperations
+    {
+        public static IEnumerable<string> ExtractByEmail(params Students[] students)
+        {
+            var result =
+                from student in students
+                where student.Email.Contains("abv.bg") == true
+                select student.FirstName + " " + student.LastName;
+
+            return result;
+        }
+    }
+}
---
+++ 11_ExtractStudentsByEmail/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SharedClassStudents.Models;
+using _11_ExtractStudentsByEmail.Models;
+
+
+namespace _11_ExtractStudentsByEmail
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Students[] students = new[]
+            {
+                new Students { FirstName = "Ivan", LastName = "Dimov", Email = "ivan_dimov@gmail.com", GroupNumber = 2 },
+                new Students { FirstName = "Vera", LastName = "Georgieva", Email = "vg@abv.bg", GroupNumber = 1 },
+                new Students { FirstName = "Dragan", LastName = "Tsankov", Email = "mr_dragan@outlook.com", GroupNumber = 1 },
+                new Students { FirstName = "Khan", LastName = "Asparuh", Email = "asparuh@gmail.com", GroupNumber = 2 },
+                new Students { FirstName = "Vera", LastName = "Aleksieva", Email = "vera@abv.bg", GroupNumber = 3 },
+                new Students { FirstName = "Jon", LastName = "Brown", Email = "jbrown@hotmail.com", GroupNumber = 4 },
+                new Students { FirstName = "Ivan", LastName = "Stefanov", Email = "ivan@abv.bg", GroupNumber = 2 }
+            };
+
+            var selectByEmail = QueryOperations.ExtractByEmail(students);
+
+            Console.WriteLine(string.Join("\n", selectByEmail));
+        }
+    }
+}
