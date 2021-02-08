e25bfe48b972e1b9ec3a4f9190778a79def7f9a2
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-02-08 03:02:39 +0200

    complete problem 12

---
+++ 12_ExtractStudentsByPhone/Models/QueryOperations.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SharedClassStudents.Models;
+
+namespace _12_ExtractStudentsByPhone.Models
+{
+    class QueryOperations
+    {
+        public static IEnumerable<string> GroupByTelFromSofia(params Students[] students)
+        {
+            var result =
+                from student in students
+                where student.Tel.Contains("-02-") == true
+                select student.FirstName + " " + student.LastName;
+
+            return result;
+        }
+    }
+}
---
+++ 12_ExtractStudentsByPhone/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SharedClassStudents.Models;
+using _12_ExtractStudentsByPhone.Models;
+
+namespace _12_ExtractStudentsByPhone
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Students[] students = new[]
+            {
+                new Students { FirstName = "Ivan", LastName = "Dimov", Tel = "+359-056-235486", Email = "ivan_dimov@gmail.com", GroupNumber = 2 },
+                new Students { FirstName = "Dragan", LastName = "Tsankov", Tel = "+359-02-487956", Email = "mr_dragan@outlook.com", GroupNumber = 1 },
+                new Students { FirstName = "Vera", LastName = "Georgieva", Tel = "+359-068-203154", Email = "vg@abv.bg", GroupNumber = 1 },
+                new Students { FirstName = "Vera", LastName = "Aleksieva", Tel = "+359-02-458565", Email = "vera@abv.bg", GroupNumber = 3 },
+                new Students { FirstName = "Khan", LastName = "Asparuh", Tel = "+359-056-666589", Email = "asparuh@gmail.com", GroupNumber = 2 },
+                new Students { FirstName = "Jon", LastName = "Brown", Tel = "+359-02-444889", Email = "jbrown@hotmail.com", GroupNumber = 4 },
+                new Students { FirstName = "Ivan", LastName = "Stefanov", Tel = "+359-02-444111", Email = "ivan@abv.bg", GroupNumber = 2 }
+            };
+
+            var takeAllContainSophiaTels = QueryOperations.GroupByTelFromSofia(students);
+
+            Console.WriteLine(string.Join("\n", takeAllContainSophiaTels));
+        }
+    }
+}
