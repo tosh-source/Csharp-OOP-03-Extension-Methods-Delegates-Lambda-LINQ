3477ee0508645bba1d5ff8379e550a91cba9775d
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-02-10 03:14:58 +0200

    clean unneeded usings

--- 13_ExtractStudentsByMarks/Models/QueryOperations.cs
+++ 13_ExtractStudentsByMarks/Models/QueryOperations.cs
@@ -2,8 +2,6 @@
 using SharedClassStudents.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _13_ExtractStudentsByMarks.Models
 {
