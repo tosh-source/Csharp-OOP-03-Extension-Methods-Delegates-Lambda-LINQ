881fc84994bef5acb89afa886cbb235c89aec4d4
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-02-10 22:29:59 +0200

    complete problem 15

---
+++ 15_ExtractMarks/Models/QueryOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SharedClassStudents.Models;
+
+namespace _15_ExtractMarks.Models
+{
+    class QueryOperations : Students
+    {
+        public static IEnumerable<string> ExtractMarks(string yearOfEnrolling = "2006", params Students[] students)
+        {
+            if (yearOfEnrolling.Length != 4)
+            {
+                throw new ArgumentException("Wrong format! Year example: 2006");
+            }
+
+            string enrolledYear = yearOfEnrolling.Remove(0, 2);  //remove first two elements
+
+            var result = students.Where(student => student.FacultyNumber.Contains(enrolledYear))
+                                 .Select(student => student.FirstName + " " + student.LastName 
+                                         + ": " + string.Join(", ", student.Mark));
+
+            return result;
+        }
+    }
+}
---
+++ 15_ExtractMarks/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _15_ExtractMarks.Models;
+using SharedClassStudents.Models;
+
+namespace _15_ExtractMarks
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Students[] students =
+            {
+                new Students { FacultyNumber = "123406", FirstName = "Ivan", LastName = "Dimov", Mark = new List<int>() { 4, 2} },
+                new Students { FacultyNumber = "586007", FirstName = "Vera", LastName = "Aleksieva", Mark = new List<int>() { 2, 5} },
+                new Students { FacultyNumber = "453509", FirstName = "Khan", LastName = "Asparuh", Mark = new List<int>() { 3, 3} },
+                new Students { FacultyNumber = "222206", FirstName = "Dragan", LastName = "Tsankov", Mark = new List<int>() { 3, 2} },
+                new Students { FacultyNumber = "435314", FirstName = "Jon", LastName = "Brown", Mark = new List<int>() { 6, 2} },
+                new Students { FacultyNumber = "424306", FirstName = "Ivan", LastName = "Stefanov", Mark = new List<int>() { 2, 5, 5} },
+                new Students { FacultyNumber = "785406", FirstName = "Vera", LastName = "Georgieva", Mark = new List<int>() { 3} }
+            };
+
+            var studentsEnrolledYear = "2006";
+            var takeMarksEnrolledIn2006 = QueryOperations.ExtractMarks(studentsEnrolledYear, students);
+
+            Console.WriteLine("Extract all `Marks` of the students that enrolled in 2006:");
+            Console.WriteLine(string.Join("\n", takeMarksEnrolledIn2006));
+        }
+    }
+}
