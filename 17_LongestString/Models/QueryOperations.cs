b383e59d3eb88e0d526a18560d20ebd5ac3b6a69
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-02-15 01:33:44 +0200

    complete problem 17

---
+++ 17_LongestString/Models/QueryOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _17_LongestString.Models
+{
+    class QueryOperations
+    {
+        public static string ReturnLongestString(params string[] stringArray)
+        {
+            var result =
+                (from currentString in stringArray
+                 orderby currentString.Length
+                 select currentString).Last();
+
+            return result;
+        }
+    }
+}
---
+++ 17_LongestString/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using _17_LongestString.Models;
+
+namespace _17_LongestString
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            var stringArray = new[]
+            {
+                "By",
+                "By using delegates develop an universal static method to calculate",
+                "By using delegates develop an universal static",
+                "By using delegates",
+                "By using delegates develop an universal static method to calculate the sum.",
+                "By using delegates develop an"
+            };
+
+            var result = QueryOperations.ReturnLongestString(stringArray);
+            Console.WriteLine(result);
+        }
+    }
+}
