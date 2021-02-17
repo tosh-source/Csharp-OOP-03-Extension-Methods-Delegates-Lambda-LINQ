12b85f582c44f854e4d8ad2455e2ea77815e595a
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-02-17 03:30:38 +0200

    complete problem 18

---
+++ 18_GroupedByGroupNumber/Models/QueryOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using SharedClassStudents.Models;
+
+namespace _18_GroupedByGroupNumber.Models
+{
+    static class QueryOperations
+    {
+        public static IEnumerable<IGrouping<int, Students>> GroupStudents(params Students[] students)
+        {
+            var result =
+                from student in students
+                orderby student.GroupNumber
+                group student by student.GroupNumber;
+
+            return result;
+        }
+    }
+}
