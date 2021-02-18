8af1ed6f9178abf85e1631dcbea8f6a582114d79
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-02-18 16:36:05 +0200

    complete problem 19

---
+++ 19_GroupedByGroupNumberЕxtensions/Models/QueryOperations.cs
@@ -0,0 +1,18 @@
+﻿using SharedClassStudents.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _19_GroupedByGroupNumberЕxtensions.Models
+{
+    class QueryOperations
+    {
+        public static IEnumerable<IGrouping<int, Students>> GroupStudents(params Students[] students)
+        {
+            var result = students.OrderBy(student => student.GroupNumber)
+                                 .GroupBy(student => student.GroupNumber);
+
+            return result;
+        }
+    }
+}
---
+++ 19_GroupedByGroupNumberЕxtensions/Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _19_GroupedByGroupNumberЕxtensions.Models;
+using SharedClassStudents.Models;
+
+
+namespace _19_GroupedByGroupNumberЕxtensions
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Students[] students =
+{
+                new Students { FirstName = "Ivan", LastName = "Dimov", Email = "ivan_dimov@gmail.com", GroupNumber = 2 },
+                new Students { FirstName = "Vera", LastName = "Georgieva", Email = "vg@abv.bg", GroupNumber = 1 },
+                new Students { FirstName = "Dragan", LastName = "Tsankov", Email = "mr_dragan@outlook.com", GroupNumber = 5 },
+                new Students { FirstName = "Khan", LastName = "Asparuh", Email = "asparuh@gmail.com", GroupNumber = 6 },
+                new Students { FirstName = "Vera", LastName = "Aleksieva", Email = "vera@abv.bg", GroupNumber = 3 },
+                new Students { FirstName = "Jon", LastName = "Brown", Email = "jbrown@hotmail.com", GroupNumber = 4 },
+                new Students { FirstName = "Ivan", LastName = "Stefanov", Email = "ivan@abv.bg", GroupNumber = 7 }
+            };
+
+            var studentGroups = QueryOperations.GroupStudents(students);
+
+            foreach (var currentGroup in studentGroups)
+            {
+                Console.WriteLine(currentGroup.Key);
+
+                foreach (var currentStudent in currentGroup)
+                {
+                    Console.WriteLine(currentStudent.FirstName + " " + currentStudent.LastName);
+                }
+            }
+        }
+    }
+}
