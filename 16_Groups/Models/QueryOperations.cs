21169e46c8059635464a60e38ed27658595b90d9
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-06-09 01:55:32 +0300

    add needed query as method

--- 16_Groups/Models/QueryOperations.cs
+++ 16_Groups/Models/QueryOperations.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharedClassStudents.Models;
 
 namespace _16_Groups.Models
 {
     class QueryOperations
     {
+        public static IEnumerable GroupDepartment(Group[] groupOfDepartments, string groupByDepartment = "Mathematics", params Students[] students)
+        {
+            var groupDepartment =
+                from currentStudent in students
+                join currentGroup in groupOfDepartments
+                on currentStudent.GroupNumber equals currentGroup.GroupNumber
+                where currentGroup.DepartmentName == groupByDepartment
+                select new
+                {
+                    Student = currentStudent.FirstName + " " + currentStudent.LastName,
+                    Department = currentGroup.DepartmentName
+                };
+
+            return groupDepartment;
+        }
     }
 }
