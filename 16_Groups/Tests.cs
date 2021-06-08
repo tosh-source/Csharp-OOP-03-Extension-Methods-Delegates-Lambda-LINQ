1c7dece7e8f4351765e89a077c2bef55f999c153
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-06-09 00:29:07 +0300

    add empty project

---
+++ 16_Groups/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Groups
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
